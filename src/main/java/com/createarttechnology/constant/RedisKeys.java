e1f124c41a5bf75e8a1784f1e327769bd36b31e5
XuhuiLee <lxh199108@gmail.com>
2019-07-23 18:50:26 +0800

    迁移常量

---
+++ src/main/java/com/createarttechnology/constant/RedisKeys.java
@@ -0,0 +1,11 @@
+package com.createarttechnology.constant;
+
+/**
+ * Created by lixuhui on 2019/7/23.
+ */
+public class RedisKeys {
+
+    public static final String REDIS_UID_PREFIX = "uid_";
+    public static final String REDIS_ARTICLE_PV_HASH = "blg_ar_pv_h";
+
+}
---
+++ src/main/java/com/createarttechnology/constant/StringConstant.java
@@ -0,0 +1,14 @@
+package com.createarttechnology.constant;
+
+/**
+ * Created by lixuhui on 2019/7/23.
+ */
+public final class StringConstant {
+
+    public static final String REQUEST_ATTRIBUTE_IS_BOT = "IS_BOT";
+
+    public static final String COOKIE_LOGIN_KEY = "cat_lk";
+    public static final String COOKIE_LOGIN_TOKEN = "cat_ltk";
+    public static final String COOKIE_USER_UUID = "cat_uuid";
+
+}
